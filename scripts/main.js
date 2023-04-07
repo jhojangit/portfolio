8c805fb56b3b3984c285f477cad2abcba50f8493
jhojangit <jhojanblackout@gmail.com>
2023-04-07 11:09:57 -0500

    lista lógica, presentación básica y projects básico

---
+++ scripts/main.js
@@ -0,0 +1,56 @@
+const handleNav = () =>{
+
+
+    const projectsNavHTML = document.querySelector('.projects_nav')
+    const projectsHTML = document.querySelector('.projects_container')
+
+    const contactNavHTML = document.querySelector('.contact_nav')
+    const contactHTML = document.querySelector('.contact_container')
+    const spanHTML = document.querySelector('.span_to_projects')
+
+
+
+    const presentationNavHTML = document.querySelector('.presentation_nav')
+    const presentationHTML = document.querySelector('.presentation_container')
+
+
+
+
+
+    projectsNavHTML.addEventListener('click', () =>{
+        projectsHTML.classList.add('projects_view')
+        contactHTML.classList.remove('contact_view')
+        presentationHTML.classList.add('presentation_view_initial')
+    })  
+
+    spanHTML.addEventListener('click', () =>{
+        projectsHTML.classList.add('projects_view')
+        contactHTML.classList.remove('contact_view')
+        presentationHTML.classList.add('presentation_view_initial')
+    }) 
+
+
+    presentationNavHTML.addEventListener('click', () =>{
+        presentationHTML.classList.add('presentation_view')
+        contactHTML.classList.remove('contact_view')
+        projectsHTML.classList.remove('projects_view')
+        presentationHTML.classList.remove('presentation_view_initial')
+    })  
+
+
+    contactNavHTML.addEventListener('click', () =>{
+        contactHTML.classList.add('contact_view')
+        projectsHTML.classList.remove('projects_view')
+        presentationHTML.classList.remove('presentation_view')
+        presentationHTML.classList.remove('presentation_view_initial')
+
+    })  
+}
+
+
+
+const main = () =>{
+    handleNav()
+}
+
+main()
