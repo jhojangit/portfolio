cf0761d0c1c4349acc616bce5e4adf0a3c084a5b
RazielBlaster <jhojanblackout@gmail.com>
2023-09-14 19:06:18 -0500

    Fixed title animation

--- scripts/load.js
+++ scripts/load.js
@@ -1,5 +1,8 @@
 function loading(){
     const loadHTML = document.querySelector(".load_container")
+    const loadAddClassPresentation = document.querySelector(".presentation_info")
+
 
     loadHTML.classList.add("load_full")
+    loadAddClassPresentation.classList.add("text-focus-in")
 }
